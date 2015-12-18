870249b7377007fe388e8537a34df59957d15b36
Thomas Tursics <thomas@tursics.de>
2015-12-19 00:16:23 +0100

    added a flipping card

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,43 @@
 //-----------------------------------------------------------------------
 
+function createNewCard(front,back,color)
+{
+	var str = '<div id="card"><figure class="front">'+front+'</figure><figure class="back">'+back+'</figure></div>';
+
+	$( '<section>', {
+		class: 'container '/*+color*/,
+		html: str
+	})
+	.appendTo( $('#board') )
+	.click( function() {
+		$(this).children().first().toggleClass('flipped');
+	});
+}
+
+//-----------------------------------------------------------------------
+
 $( document ).ready( function() {
 	$( '<div>', {
-		class: 'hexagon',
-		text: 'Test'
+		class: 'hexagon red',
+		text: 'Test 1'
 	}).appendTo( $('#board') );
+
+	$( '<div>', {
+		class: 'hexagon blue',
+		text: 'Test 2'
+	}).appendTo( $('#board') );
+
+	$( '<div>', {
+		class: 'hexagon red',
+		text: 'Test 3'
+	}).appendTo( $('#board') );
+
+	$( '<div>', {
+		class: 'hexagon blue',
+		text: 'Test 4'
+	}).appendTo( $('#board') );
+
+	createNewCard('Front text','Back text','red');
 });
 
 //-----------------------------------------------------------------------
