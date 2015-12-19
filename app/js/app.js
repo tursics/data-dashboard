91b95e0d29c12329400cd93d82863edd87b27841
Thomas Tursics <thomas@tursics.de>
2015-12-19 02:54:33 +0100

    changed angels of hexagon (to match code-for-sticker)

--- app/js/app.js
+++ app/js/app.js
@@ -52,14 +52,26 @@ function finishBoard()
 //-----------------------------------------------------------------------
 
 $(document).ready(function() {
-	createNewCard('Text 1','Red','red');
-	createNewCard('Text 2','Blue','blue');
-	createNewCard('Text 3','Red','red');
-	createNewCard('Text 4','Blue','blue');
-	createNewCard('Text 5','Red','red');
-	createNewCard('Text 6','Blue','blue');
-	createNewCard('Text 7','Red','red');
-	createNewCard('Text 8','Blue','blue');
+	createNewCard('<img src="./img/codefor_berlin.png" style="width:100%;margin-top:-3.6em;">','Berlin','transparent');
+	createNewCard('<img src="./img/codefor_bonn.png" style="width:100%;margin-top:-3.6em;">','Bonn','transparent');
+	createNewCard('<img src="./img/codefor_chemnitz.png" style="width:100%;margin-top:-3.6em;">','Chemnitz','transparent');
+	createNewCard('<img src="./img/codefor_dresden.png" style="width:100%;margin-top:-3.6em;">','Dresden','transparent');
+	createNewCard('<img src="./img/codefor_frankfurt.png" style="width:100%;margin-top:-3.6em;">','Frankfurt','transparent');
+	createNewCard('<img src="./img/codefor_giessen.png" style="width:100%;margin-top:-3.6em;">','Gießen','transparent');
+	createNewCard('<img src="./img/codefor_hamburg.png" style="width:100%;margin-top:-3.6em;">','Hamburg','transparent');
+	createNewCard('<img src="./img/codefor_heilbronn.png" style="width:100%;margin-top:-3.6em;">','Heilbronn','transparent');
+	createNewCard('<img src="./img/codefor_jena.png" style="width:100%;margin-top:-3.6em;">','Jena','transparent');
+	createNewCard('<img src="./img/codefor_karlsruhe.png" style="width:100%;margin-top:-3.6em;">','Karlsruhe','transparent');
+	createNewCard('<img src="./img/codefor_koeln.png" style="width:100%;margin-top:-3.6em;">','Köln','transparent');
+	createNewCard('<img src="./img/codefor_leipzig.png" style="width:100%;margin-top:-3.6em;">','Leipzig','transparent');
+	createNewCard('<img src="./img/codefor_magdeburg.png" style="width:100%;margin-top:-3.6em;">','Magdeburg','transparent');
+	createNewCard('<img src="./img/codefor_muenchen.png" style="width:100%;margin-top:-3.6em;">','München','transparent');
+	createNewCard('<img src="./img/codefor_muenster.png" style="width:100%;margin-top:-3.6em;">','Münster','transparent');
+	createNewCard('<img src="./img/codefor_stuttgart.png" style="width:100%;margin-top:-3.6em;">','Stuttgart','transparent');
+	createNewCard('<img src="./img/codefor_ulm.png" style="width:100%;margin-top:-3.6em;">','Ulm','transparent');
+	createNewCard('<img src="./img/codefor_wuppertal.png" style="width:100%;margin-top:-3.6em;">','Wuppertal','transparent');
+	createNewCard('Text a','Red','red');
+	createNewCard('Text b','Blue','blue');
 
 	finishBoard();
 });
