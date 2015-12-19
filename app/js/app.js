d10c1d099ad44cbc8aed387035bb37a3439a08b4
Thomas Tursics <thomas@tursics.de>
2015-12-19 01:55:46 +0100

    added Safari support + a little space around the cards + resize event (responsibility)

--- app/js/app.js
+++ app/js/app.js
@@ -8,19 +8,60 @@ function createNewCard(front,back,color)
 		class: 'card '+color,
 		html: str
 	})
-	.appendTo( $('#board') )
-	.click( function() {
+	.appendTo($('#board'))
+	.click(function() {
 		$(this).children().first().toggleClass('flipped');
 	});
 }
 
 //-----------------------------------------------------------------------
 
-$( document ).ready( function() {
+function finishBoard()
+{
+	$( '<section>', {
+		class: 'endcard'
+	})
+	.appendTo($('#board'));
+
+	var resizeTimer;
+	$(window).resize(function() {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(recalcPositions, 100);
+	});
+
+	function recalcPositions() {
+		var elements = $('#board').children();
+		var lastTop;
+		var y = 0;
+		for(var i = 0; i < (elements.length-1); ++i) {
+			var e = $(elements[i]);
+			var top = parseInt(e.position().top);
+			if((0===i) || (lastTop === top)) {
+				e.css('margin-left','0');
+			} else {
+				++y;
+				e.css('margin-left',y%2?'6.55em':'0');
+			}
+			lastTop = top;
+		};
+	};
+
+	$(window).trigger('resize');
+}
+
+//-----------------------------------------------------------------------
+
+$(document).ready(function() {
 	createNewCard('Text 1','Red','red');
 	createNewCard('Text 2','Blue','blue');
 	createNewCard('Text 3','Red','red');
 	createNewCard('Text 4','Blue','blue');
+	createNewCard('Text 5','Red','red');
+	createNewCard('Text 6','Blue','blue');
+	createNewCard('Text 7','Red','red');
+	createNewCard('Text 8','Blue','blue');
+
+	finishBoard();
 });
 
 //-----------------------------------------------------------------------
