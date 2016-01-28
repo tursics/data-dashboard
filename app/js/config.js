1adcd31fc5f231dfc8df914e9ca95db6b8715ce6
Thomas Tursics <thomas@tursics.de>
2016-01-28 22:11:58 +0100

    added a config file

---
+++ app/js/config.js
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------
+
+var config = config || {
+	city: "Berlin",
+	cards: [
+		'berlin/berlin-opendata.json',
+		'berlin/code-for-berlin.json',
+		'berlin/help.json',
+		'berlin/doppelhaushalt-20162017.json',
+		'berlin/angebote-zur-unterstuetzung-von-fluechtlingen.json',
+		'berlin/aktuelle-ausschreibungen-nach-vol-vob-vof-oder-fuer-interessenbekundungsverfahren-0.json',
+	]
+};
+
+//-----------------------------------------------------------------------
