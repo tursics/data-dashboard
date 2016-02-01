71a67d010d601ab05182ca71f0ccd318f15d2e96
Thomas Tursics <thomas@tursics.de>
2016-02-01 18:14:46 +0100

    added bezirksamtsbeschluesse.json

--- app/js/config.js
+++ app/js/config.js
@@ -6,9 +6,22 @@ var config = config || {
 		'berlin/berlin-opendata.json',
 		'berlin/code-for-berlin.json',
 		'berlin/help.json',
+
+		// datasets from http://daten.berlin.de/datensaetze
 		'berlin/doppelhaushalt-20162017.json',
 		'berlin/angebote-zur-unterstuetzung-von-fluechtlingen.json',
 		'berlin/aktuelle-ausschreibungen-nach-vol-vob-vof-oder-fuer-interessenbekundungsverfahren-0.json',
+		'berlin/bezirksamtsbeschluesse.json',
+
+		// other data from 2016
+//		'berlin/angebote-fuer-kinder-jugend-und-familie.json',
+//		'berlin/kamerale-monatsdaten-bezirk-lichtenberg-2015.json',
+//		'berlin/haushaltsplan-des-bezirksamtes-lichtenberg-von-berlin-fuer-20162017.json',
+//		'berlin/wochen-und-troedelmaerkte.json',
+//		'berlin/daten-zur-arbeitslosigkeit-fuer-opendata.json',
+//		'berlin/beschaeftigungsdaten-fuer-opendata.json',
+
+		// and many more data from 2015 and older
 	]
 };
 
