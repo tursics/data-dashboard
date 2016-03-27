7c46fb6844a9de4d19ab71e9a39de3b1fb18b897
Thomas Tursics <thomas@tursics.de>
2016-03-28 00:24:47 +0200

    JSLint my code

--- app/js/about.js
+++ app/js/about.js
@@ -1,49 +1,53 @@
+/*jslint browser: true*/
+/*global $,config,dict*/
+
 //-----------------------------------------------------------------------
 
-function about()
-{
-	function sortLicense( left, right) {
-		if( left.licenseURL == right.licenseURL) {
+function about() {
+	var max = (0 === cityConfig.cards.length ? 1 : cityConfig.cards.length),
+		fine = 0,
+		error = 0,
+		str = '';
+
+	function sortLicense(left, right) {
+		if (left.licenseURL === right.licenseURL) {
 			return (left.attribution > right.attribution) ? 1 : -1;
 		}
 		return left.licenseURL > right.licenseURL ? 1 : -1;
 	}
 
-	function updateBars()
-	{
-		var sum = 0;
+	function updateBars() {
+		var sum = 0,
+			err = (error * 100 / max),
+			fin = (fine * 100 / max);
 
-		var err = (error*100/max);
-		$('#progressDanger').text((parseInt(err * 10) / 10) + '%' + (err > 20 ? ' '+dict['progressCorrupt'] : ''));
-		if((0<err) && (err<3)) {
+		$('#progressDanger').text((parseInt(err * 10) / 10) + '%' + (err > 20 ? ' ' + dict.progressCorrupt : ''));
+		if ((0 < err) && (err < 3)) {
 			err = 3;
 		}
 		sum += err;
 
-		var fin = (fine*100/max);
 		$('#progressSuccess').text((parseInt(fin * 10) / 10) + '%');
-		if((0<fin) && (fin<3)) {
+		if ((0 < fin) && (fin < 3)) {
 			fin = 3;
 		}
 		sum += fin;
 
-		$('#progressBar').css('width', (100-Math.max(sum,100))+'%');
-		$('#progressSuccess').css('width', fin+'%');
-		$('#progressDanger').css('width', err+'%');
+		$('#progressBar').css('width', (100 - Math.max(sum, 100)) + '%');
+		$('#progressSuccess').css('width', fin + '%');
+		$('#progressDanger').css('width', err + '%');
 	}
 
-	function showLicenseTable()
-	{
-		function addLicense(url, attributions)
-		{
-			var str = '';
+	function showLicenseTable() {
+		function addLicense(url, attributions) {
+			var str = '',
+				license = url;
 
-			if(attribution.length > 0) {
-				var license = url;
-				if('http://creativecommons.org/licenses/by/3.0/de/' == url) {
-					license = dict['licenseCCBY'];
-				} else if('http://creativecommons.org/licenses/by-sa/3.0/de/' == url) {
-					license = dict['licenseCCBYSA'];
+			if (attributions.length > 0) {
+				if ('http://creativecommons.org/licenses/by/3.0/de/' === url) {
+					license = dict.licenseCCBY;
+				} else if ('http://creativecommons.org/licenses/by-sa/3.0/de/' === url) {
+					license = dict.licenseCCBYSA;
 				}
 				str += '<div class="panel-heading"><h3 class="panel-title">' + license + '</h3></div>';
 				str += '<div class="panel-body">';
@@ -54,21 +58,22 @@ function about()
 			return str;
 		}
 
-		var str = '';
+		var str = '',
+			currectLicence = '',
+			currentAttribution = '',
+			attribution = '',
+			i = 0;
 
-		config.feed.sort( sortLicense);
+		config.feed.sort(sortLicense);
 
-		var currectLicence = '';
-		var currentAttribution = '';
-		var attribution = '';
-		for(var i = 0; i < config.feed.length; ++i) {
-			if(config.feed[i].licenseURL != currectLicence) {
+		for (i = 0; i < config.feed.length; ++i) {
+			if (config.feed[i].licenseURL !== currectLicence) {
 				str += addLicense(currectLicence, attribution);
 				currectLicence = config.feed[i].licenseURL;
 				currentAttribution = config.feed[i].attribution;
 				attribution = '"' + config.feed[i].attribution + '"';
 			} else {
-				if(currentAttribution != config.feed[i].attribution) {
+				if (currentAttribution !== config.feed[i].attribution) {
 					currentAttribution = config.feed[i].attribution;
 					attribution += ', "' + config.feed[i].attribution + '"';
 				}
@@ -76,13 +81,11 @@ function about()
 		}
 		str += addLicense(currectLicence, attribution);
 
-		$('#licensetable').html( str);
+		$('#licensetable').html(str);
 	}
 
-	function readCardInfos()
-	{
-		function colorizeCard(data)
-		{
+	function readCardInfos() {
+		function colorizeCard(data) {
 			data = data || {};
 			data.portal = data.portal || {};
 //			data.portal.license = data.portal.license || '';
@@ -100,33 +103,33 @@ function about()
 			++fine;
 		}
 
-		if(config.loaded < cityConfig.cards.length) {
+		if (config.loaded < cityConfig.cards.length) {
 			var url = cityConfig.cards[config.loaded];
 			$.ajax(url)
-			.done(function(json){
-				var data = jQuery.parseJSON(json);
-				colorizeCard(data);
-			})
-			.fail(function(jqXHR, textStatus){
-				if('parsererror'==textStatus) {
-					try {
-						var data = jQuery.parseJSON(jqXHR.responseText);
-						if( typeof data.location != 'undefined') {
-							colorizeCard(data);
-							return;
+				.done(function (json) {
+					var data = $.parseJSON(json);
+					colorizeCard(data);
+				})
+				.fail(function (jqXHR, textStatus) {
+					if ('parsererror' === textStatus) {
+						try {
+							var data = $.parseJSON(jqXHR.responseText);
+							if (typeof data.location !== 'undefined') {
+								colorizeCard(data);
+								return;
+							}
+						} catch (e) {
 						}
-					} catch(e) {
 					}
-				}
-				++error;
-			})
-			.always(function(){
-				if(config.loadLicenses) {
-					updateBars();
-					++config.loaded;
-					readCardInfos();
-				}
-			});
+					++error;
+				})
+				.always(function () {
+					if (config.loadLicenses) {
+						updateBars();
+						++config.loaded;
+						readCardInfos();
+					}
+				});
 		} else {
 			fine = max;
 			updateBars();
@@ -135,20 +138,19 @@ function about()
 		}
 	}
 
-	var str = '';
 	str += '<div class="panel panel-info">';
-	str += '<div class="panel-heading"><h3 class="panel-title">'+dict['aboutTitle']+'</h3></div>';
+	str += '<div class="panel-heading"><h3 class="panel-title">' + dict.aboutTitle + '</h3></div>';
 	str += '<div class="panel-body">';
 
-	str += '<p>'+dict['aboutIntroduction']+'</p>';
-	str += '<p>'+dict['aboutHexagon']+'</p>';
-	str += '<p>'+dict['aboutFeedback']+'</p>';
-	str += '<p>'+dict['aboutLicense']+'</p>';
-	str += '<p>'+dict['aboutContribute']+'</p>';
-	str += '<p>'+dict['aboutAttribution']+'</p>';
+	str += '<p>' + dict.aboutIntroduction + '</p>';
+	str += '<p>' + dict.aboutHexagon + '</p>';
+	str += '<p>' + dict.aboutFeedback + '</p>';
+	str += '<p>' + dict.aboutLicense + '</p>';
+	str += '<p>' + dict.aboutContribute + '</p>';
+	str += '<p>' + dict.aboutAttribution + '</p>';
 
 	str += '<div class="panel panel-info" id="licensetable">';
-	str += '<div class="panel-heading"><h3 class="panel-title">'+dict['aboutCreateAttributions']+'</h3></div>';
+	str += '<div class="panel-heading"><h3 class="panel-title">' + dict.aboutCreateAttributions + '</h3></div>';
 	str += '<div class="panel-body">';
 
 	str += '<div class="progress">';
@@ -163,11 +165,7 @@ function about()
 	str += '</div>';
 	str += '</div>';
 
-	$('#build').html( str);
-
-	var max = (0==cityConfig.cards.length ? 1 : cityConfig.cards.length);
-	var fine = 0;
-	var error = 0;
+	$('#build').html(str);
 
 	updateBars();
 
