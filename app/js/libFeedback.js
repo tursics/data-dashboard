ec87c4672f3a506dd38155ccb7f18ea48e441aaa
Thomas Tursics <thomas@tursics.de>
2017-06-06 00:41:20 +0200

    added click event handler

--- app/js/libFeedback.js
+++ app/js/libFeedback.js
@@ -11,13 +11,14 @@
 		var settings = $.extend({
 			position: 'relative',
 			at: 'top right',
-			iconSize: 3
+			background: '#265a88',
+			color: 'white',
+			iconSize: 2.5,
+			iconFace: 'glyphicon-envelope'
 		}, options),
 			snippet =
-			'<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">' +
-			'<div style="background:white;color:black;font-size:' + parseFloat(settings.iconSize) + 'em;position:absolute;top:0;right:0;border-radius:1em;">' +
-			'<div style="display:block;width:1.5em;height:1.5em;line-height:1.5em;text-align:center;"><i class="fa fa-envelope" aria-hidden="true"></i><span class="glyphicon glyphicon-search" aria-hidden="true"></span></div' +
-			'<button type="button" class="btn btn-primary dropdown-toggle btn-circle btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bug fa-2x" title="Report Bug"></i></button>' +
+			'<div style="background:' + settings.background + ';color:' + settings.color + ';font-size:' + parseFloat(settings.iconSize) + 'em;position:absolute;top:0;right:0;border-radius:1em;" class="feedback-button">' +
+			'<div style="display:block;width:2em;height:2em;line-height:2.25em;text-align:center;"><span class="glyphicon ' + settings.iconFace + '" aria-hidden="true"></span></div>' +
 			'</div>';
 
 		return this.each(function (id, elem) {
@@ -25,6 +26,10 @@
 
 			obj.css('position', 'relative');
 			obj.prepend(snippet);
+			obj.find('.feedback-button').on('click', function (e) {
+				e.stopPropagation();
+				console.log('x');
+			});
 		});
 	};
 
