d042dad69c3c94464e2e02fcbe9aab8100733ced
Thomas Tursics <thomas@tursics.de>
2017-07-30 23:04:26 +0200

    API update

--- app/deutschebahn/fe68e74c-f2d2-4645-b789-015f204f2df6.php
+++ app/deutschebahn/fe68e74c-f2d2-4645-b789-015f204f2df6.php
@@ -1,4 +1,4 @@
-<?php header( 'Access-Control-Allow-Origin: *'); ?>
+<?php header( 'Access-Control-Allow-Origin: *'); header('Content-Type: application/json'); ?>
 {
 	"location": {
 		"country": "Germany",
@@ -6,8 +6,8 @@
 	},
 	"portal": {
 		"url": "http://data.deutschebahn.com/dataset/fe68e74c-f2d2-4645-b789-015f204f2df6",
-		"title": "ADAM",
-		"description": "Betriebszustand von Aufzügen.",
+		"title": "ADAM (Vorgänger der FaSta-API)",
+		"description": "Betriebszustand von Aufzügen. (Wird durch erweiterte FaSta-API abgelöst und am 31.3.2017 abgeschaltet.)",
 		"license": "Creative Commons Namensnennung Lizenz - unter gleichen Bedingungen",
 		"licenseURL": "http://creativecommons.org/licenses/by-sa/3.0/de/",
 		"attribution": "Michael Binzen",
@@ -19,26 +19,19 @@
 		"textTop": "Treppen laufen!",
 		"textBottom": "Aufzüge sind außer Betrieb",
 <?php
-	$url = 'http://adam.noncd.db.de/api/v1.0/facilities';
+	$url = 'https://api.deutschebahn.com/fasta/v1/facilities?state=INACTIVE';
 
-//	$json = file_get_contents( $url );
 	$curl_handle = curl_init();
 	curl_setopt( $curl_handle, CURLOPT_URL, $url );
 	curl_setopt( $curl_handle, CURLOPT_CONNECTTIMEOUT, 2 );
 	curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, 1 );
 	curl_setopt( $curl_handle, CURLOPT_USERAGENT, 'Datenwaben - datenwaben.de' );
+	curl_setopt( $curl_handle, CURLOPT_HTTPHEADER, array('Authorization: Bearer 2b344cb863ad6086779ba76dd628f9fd'));
 	$json = curl_exec( $curl_handle );
 	curl_close( $curl_handle );
 
 	$data = json_decode($json, TRUE);
-	$count = 0;
-
-	foreach( $data as $elevator ) {
-		// ACTIVE, INACTIVE, UNKNOWN
-		if( "INACTIVE" == trim($elevator['state'])) {
-			++$count;
-		}
-	}
+	$count = sizeof($data);
 
 	echo '"value": "'.$count.'",';
 ?>
